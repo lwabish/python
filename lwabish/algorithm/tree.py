73b2dc5410f7a89193d473b0f12aab06d0c852c3
lwabish <wubw@pku.edu.cn>
2021-11-01 10:32:00 +0800

    perf: tree

--- lwabish/algorithm/tree.py
+++ lwabish/algorithm/tree.py
@@ -1,6 +1,8 @@
 """
-树相关数据结构和算法
+树相关数据结构和工具函数
 """
+from collections import deque
+from typing import Optional
 
 
 class TreeNode:
@@ -14,14 +16,13 @@ class TreeNode:
         self.right = None
         self.parent = None
 
-    def __str__(self) -> str:
-        return 'TreeNode[{}]'.format(str(self.val))
-
     def __repr__(self) -> str:
         return 'TreeNode[{}]'.format(str(self.val))
 
+    __str__ = __repr__
 
-def new_tree(values: str, with_parent=False):
+
+def new_tree(values: str, with_parent=False) -> Optional[TreeNode]:
     """
     根据层次遍历字符串生成一颗二叉树，分隔符为英文逗号，空节点用字符串null表示\n
     注意：null不需要全写出来。不存在的子树下面的null不需要写\n
@@ -72,7 +73,7 @@ def new_tree(values: str, with_parent=False):
     return root
 
 
-def print_tree(root: TreeNode, prefix="", is_left=True):
+def print_tree(root: TreeNode, prefix="", is_left=True) -> None:
     """
     在控制台打印一颗二叉树，方便肉眼观察
     :param root: 根节点
@@ -88,6 +89,26 @@ def print_tree(root: TreeNode, prefix="", is_left=True):
     print(prefix + ("└── " if is_left else "┌── ") + str(root.val))
     if root.left:
         print_tree(root.left, prefix + ("    " if is_left else "│   "), True)
+
+
+def get_first_node_by_value(root: TreeNode, val: int) -> Optional[TreeNode]:
+    """
+    广度优先搜索，返回二叉树中查找到的第一个值为val的节点
+    :param root: 树的头结点
+    :param val: 目标值
+    :return: 二叉树中查找到的第一个值为val的节点
+    """
+    q = deque([])
+    q.append(root)
+    while q:
+        this_node = q.popleft()
+        if this_node.val == val:
+            return this_node
+        if this_node.left:
+            q.append(this_node.left)
+        if this_node.right:
+            q.append(this_node.right)
+    return None
 
 
 if __name__ == '__main__':
